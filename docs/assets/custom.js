583a0510b1fc1a016969dfbb1010a5c5f01569c5
Maki <mxmcube@gmail.com>
2020-03-21 09:56:11 +0000

    Automatically open all collapsable in sidebar

---
+++ docs/assets/custom.js
@@ -0,0 +1,8 @@
+document
+	.querySelectorAll(".md-nav__item--nested > .md-nav__link")
+	.forEach(el => {
+		if (
+			el.parentElement.querySelector("nav.md-nav").style.display == "none"
+		)
+			el.click();
+	});
