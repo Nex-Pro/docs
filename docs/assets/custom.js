8a904b859b516dbf188a85fcefb5dd4b82fd939f
Maki <mxmcube@gmail.com>
2020-08-15 00:48:39 +0100

    Only open nav toggles when in desktop

--- docs/assets/custom.js
+++ docs/assets/custom.js
@@ -1,5 +1,7 @@
-document
-	.querySelectorAll(".md-nav__item--nested > .md-nav__toggle")
-	.forEach(el => {
-		if (el.checked == false) el.click();
-	});
+if (window.innerWidth >= 1220) {
+	document
+		.querySelectorAll(".md-nav__item--nested > .md-nav__toggle")
+		.forEach(el => {
+			if (el.checked == false) el.click();
+		});
+}
